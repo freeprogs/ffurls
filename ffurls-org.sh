23cb117f279801379279bb1870369b57a2a0b2d1
guest <guest@localhost>
2016-11-24 19:27:42 +0000

    Add org output file shell-script

---
+++ ffurls-org.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+idir="$(ls -d $HOME/.mozilla/firefox/*.default/sessionstore-backups)"
+ifile="recovery.js"
+odir="$HOME/Downloads"
+ofname="firefox"
+ofext="org"
+
+ipath="$idir/$ifile"
+opath="$odir/$ofname.$ofext"
+
+if [ -e "$opath" ]; then
+    n=1
+    while [ -e "$opath" ]; do
+        opath="$odir/${ofname}_$n.$ofext"
+        ((n++))
+    done
+fi
+
+ffurls.py -t org "$ipath" "$opath"
+
+exit 0
