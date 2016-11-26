ac50b6acea1ded6c58115b83b63c8bd0e7728202
guest <guest@localhost>
2016-11-26 21:50:51 +0000

    Move dot to the extension in html shell-script

--- ffurls-html.sh
+++ ffurls-html.sh
@@ -4,15 +4,15 @@ idir="$(ls -d $HOME/.mozilla/firefox/*.default/sessionstore-backups)"
 ifile="recovery.js"
 odir="$HOME/Downloads"
 ofname="firefox"
-ofext="html"
+ofext=".html"
 
 ipath="$idir/$ifile"
-opath="$odir/$ofname.$ofext"
+opath="$odir/$ofname$ofext"
 
 if [ -e "$opath" ]; then
     n=1
     while [ -e "$opath" ]; do
-        opath="$odir/${ofname}_$n.$ofext"
+        opath="$odir/${ofname}_$n$ofext"
         ((n++))
     done
 fi
