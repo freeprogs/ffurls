cec4e2820707e54e174a43466c85bb771285a648
guest <guest@localhost>
2016-11-26 22:08:58 +0000

    Add description, date and author to the org shell-script

--- ffurls-org.sh
+++ ffurls-org.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# This script saves tabs opened in Firefox as title and urls pairs in
+# ~/Download/firefox.org file, using ffurls.py installed in the
+# system.
+#
+# Copyright (C) 2016, Slava <nobody@nowhere>
+
 idir="$(ls -d $HOME/.mozilla/firefox/*.default/sessionstore-backups)"
 ifile="recovery.js"
 odir="$HOME/Downloads"
