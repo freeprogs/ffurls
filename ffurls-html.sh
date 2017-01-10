8d295fb3b4c86e3c61db97f71a578e23b38e628f
guest <guest@localhost>
2017-01-10 15:40:44 +1100

    Add program templates for m4 to ffurls-html.sh

--- ffurls-html.sh
+++ ffurls-html.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# This script is a part of __PROGRAM_NAME__ __PROGRAM_VERSION__
 # This script saves tabs opened in Firefox as title and urls pairs in
 # ~/Download/firefox.html file, using ffurls.py installed in the
 # system.
 #
-# Copyright (C) 2016, Slava <nobody@nowhere>
+# __PROGRAM_COPYRIGHT__ __PROGRAM_AUTHOR__ __PROGRAM_AUTHOR_EMAIL__
 
 idir="$(ls -d $HOME/.mozilla/firefox/*.default/sessionstore-backups)"
 ifile="recovery.js"
