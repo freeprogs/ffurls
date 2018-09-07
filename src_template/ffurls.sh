e88104fec4c76166251448c684acfe35e07e427d
Slava <freeprogs.feedback@yandex.ru>
2018-09-07 20:31:18 +1100

    Add stubs for get_config_*() and extract_tabs() functions

--- src_template/ffurls.sh
+++ src_template/ffurls.sh
@@ -50,6 +50,70 @@ print_help()
     } >&2
 }
 
+# Get from config file browser directory
+# get_config_browser_dir(file)
+get_config_browser_dir()
+{
+    echo "~/.mozilla"
+}
+
+# Get from config file output directory
+# get_config_output_dir(file)
+get_config_output_dir()
+{
+    echo "~/Downloads"
+}
+
+# Get from config file output file name
+# get_config_ofname(file)
+get_config_ofname()
+{
+    echo "firefox"
+}
+
+# Get from config file output file extension for text format
+# get_config_ofext_text(file)
+get_config_ofext_text()
+{
+    echo "txt"
+}
+
+# Get from config file output file extension for org format
+# get_config_ofext_org(file)
+get_config_ofext_org()
+{
+    echo "org"
+}
+
+# Get from config file output file extension for html format
+# get_config_ofext_html(file)
+get_config_ofext_html()
+{
+    echo "html"
+}
+
+# Get from config file default output format
+# get_config_default_ofmt(file)
+get_config_default_ofmt()
+{
+    echo "org"
+}
+
+# Get tabs from browser Firefox and save them to the output file in a
+# given format.
+# extract_tabs(format,
+#              browser_dir,
+#              output_dir,
+#              output_filename,
+#              output_fileext_text,
+#              output_fileext_org,
+#              output_fileext_html)
+extract_tabs()
+{
+    echo $@
+    return 0
+}
+
 # Run main script operations
 # main([cmdarg])
 main()
@@ -59,27 +123,40 @@ main()
         return 1
     fi
     usage
+
+    config_file="/usr/local/etc/__PROGRAM_NAME__.sh"
+    browser_dir=$(get_config_browser_dir "$config_file")
+    output_dir=$(get_config_output_dir "$config_file")
+    ofname=$(get_config_ofname "$config_file")
+    ofext_text=$(get_config_ofext_text "$config_file")
+    ofext_org=$(get_config_ofext_org "$config_file")
+    ofext_html=$(get_config_ofext_html "$config_file")
+    default_ofmt=$(get_config_default_ofmt "$config_file")
     if [ $# -eq 0 ]; then
-        :
+        extract_tabs "$default_ofmt" "$browser_dir" "$output_dir" \
+                     "$ofname" "$ofext_text" "$ofext_org" "$ofext_html" || return 1
         return 0
     fi
     if [ $# -ne 0 ]; then
         case $1 in
         "--text")
-            :
+            extract_tabs "text" "$browser_dir" "$output_dir" \
+                         "$ofname" "$ofext_text" "$ofext_org" "$ofext_html" || return 1
             return 0
             ;;
         "--org")
-            :
+            extract_tabs "org" "$browser_dir" "$output_dir" \
+                         "$ofname" "$ofext_text" "$ofext_org" "$ofext_html" || return 1
             return 0
             ;;
         "--html")
-            :
+            extract_tabs "html" "$browser_dir" "$output_dir" \
+                         "$ofname" "$ofext_text" "$ofext_org" "$ofext_html" || return 1
             return 0
             ;;
         *)
-            :
-            return 0
+            error "can't recognize output format: $1"
+            return 1
             ;;
         esac
     fi
